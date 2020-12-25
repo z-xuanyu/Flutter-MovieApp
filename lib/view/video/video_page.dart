bd476c54eb69a909c1b6ec371d728607b73184b2
xuanyu <969718197@qq.com>
2020-12-25 18:43:21 +0800

    添加了视频插件

--- lib/view/video/video_page.dart
+++ lib/view/video/video_page.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:fijkplayer/fijkplayer.dart';
 
@@ -15,8 +16,8 @@ class _VideoPageState extends State<VideoPage> {
   void initState() {
     super.initState();
     player.setDataSource(
-        "https://sample-videos.com/video123/flv/240/big_buck_bunny_240p_10mb.flv",
-        autoPlay: true);
+        "https://tjc-download.weiyun.com/ftn_handler/05472e2b0bb007b617e69532b05d23a1e92b141354ad6fc4346ba77c6264ecd6/123jx.mp4",
+        autoPlay: false);
   }
 
   @override
@@ -27,6 +28,24 @@ class _VideoPageState extends State<VideoPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(child: FijkView(player: player));
+    return Stack(
+      children: [
+        Container(
+            child: FijkView(
+          player: player,
+          fit: FijkFit.fitHeight,
+          fsFit: FijkFit.fitHeight,
+        )),
+        Positioned(
+            top: MediaQueryData.fromWindow(window).padding.top + 10, // 获取状态栏高度
+            left: 180,
+            child: Center(
+              child: Text(
+                '推荐',
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
+            ))
+      ],
+    );
   }
 }
